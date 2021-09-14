db6065a9c83b3d667437fd44ec1809d2f38bda22
unknown <saidmf@rpi.edu>
2021-09-14 13:35:01 -0400

    Modified position vars in group-modify

--- scripts/group-modify.sh
+++ scripts/group-modify.sh
@@ -5,28 +5,25 @@
 # <OPTIONS>
 #  -r remove to group
 #  -a add to group
-usage() { echo "Usage: $0 [-r] [-a] </data/path> <user1>,<user2> " 1>&2; exit 1; }
-
-dest_path = $2
-users = $3 
+usage() { echo "Usage: sh group-modify.sh [-r] [-a] </data/path> <user1>,<user2>" 1>&2; exit 1; }
 
 while getopts ":ra" opt; do
   case ${opt} in
     r)
-        for ID in $(echo $users | sed "s/,/ /g")
+        for ID in $(echo $2 | sed "s/,/ /g")
         do
-            setfacl -Rx u:ID,d:u:ID dest_path
+            setfacl -Rx u:ID,d:u:ID $1
         done ;;
     a)
-        for ID in $(echo $users | sed "s/,/ /g")
+        for ID in $(echo $2 | sed "s/,/ /g")
         do
-            setfacl -Rm d:u:ID:rwX,u:ID:rwX dest_path
+            setfacl -Rm d:u:ID:rwX,u:ID:rwX $1
         done ;;
     \?) 
-        echo "Usage: $0 [-r] [-a] </data/path> <user1>,<user2> " 1>&2; exit 1 ;;
+        echo "Usage: sh group-modify.sh [-r] [-a] </data/path> <user1>,<user2>" 1>&2; exit 1 ;;
     esac 
   echo "New access list of directory" 
-  getfacl dest_path
+  getfacl $1
 done
 
 
