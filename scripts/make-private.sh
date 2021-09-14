69a285bc71f3eb198c8e4acd372aebe660831c8d
unknown <saidmf@rpi.edu>
2021-09-14 13:18:22 -0400

    updated var position

--- scripts/make-private.sh
+++ scripts/make-private.sh
@@ -2,10 +2,10 @@
 # A script to lock group out of project folder
 
 # USAGE: make-private </data/path>
-usage() { echo "Usage: $0 </data/path>" 1>&2; exit 1; }
+usage() { echo "Usage: sh $1 </data/path>" 1>&2; exit 1; }
 
-setfacl -Rm d:g::---,g::--- $2
-setfacl -Rm d:o::---,o::--- $2
+setfacl -Rm d:g::---,g::--- $3
+setfacl -Rm d:o::---,o::--- $3
 
-echo "Group is locked out of $2" 
-getfacl $2
+echo "Group is locked out of $3" 
+getfacl $3
