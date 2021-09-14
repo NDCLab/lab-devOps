0bb7dd140d68140e99b1f6e4be9621d1e12eda6e
unknown <saidmf@rpi.edu>
2021-09-14 13:16:22 -0400

    Updated make-private with proper var

--- scripts/make-private.sh
+++ scripts/make-private.sh
@@ -4,10 +4,8 @@
 # USAGE: make-private </data/path>
 usage() { echo "Usage: $0 </data/path>" 1>&2; exit 1; }
 
-dest_path = $2
-
-setfacl -Rm d:g::---,g::--- dest_path
-setfacl -Rm d:o::---,o::--- dest_path
+setfacl -Rm d:g::---,g::--- $2
+setfacl -Rm d:o::---,o::--- $2
 
 echo "Group is locked out of $2" 
-getfacl dest_path
+getfacl $2
