a456160887b65e1a98abcebda299df53bbe18e76
unknown <saidmf@rpi.edu>
2021-09-14 13:24:32 -0400

    Changed pos variable to 1

--- scripts/make-private.sh
+++ scripts/make-private.sh
@@ -2,10 +2,10 @@
 # A script to lock group out of project folder
 
 # USAGE: make-private </data/path>
-usage() { echo "Usage: sh $1 </data/path>" 1>&2; exit 1; }
+usage() { echo "Usage: sh make-private.sh </data/path>" 1>&2; exit 1; }
 
-setfacl -Rm d:g::---,g::--- $3
-setfacl -Rm d:o::---,o::--- $3
+setfacl -Rm d:g::---,g::--- $1
+setfacl -Rm d:o::---,o::--- $1
 
-echo "Group is locked out of $3" 
-getfacl $3
+echo "Group is locked out of $1" 
+getfacl $1
