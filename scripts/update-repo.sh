d58ce5b6f90b2e5468b9ec4d3662ad9b9bf32ee1
Farukh Said <saidmf@rpi.edu>
2021-10-04 12:30:59 -0400

    Added additional loops for other folders

--- scripts/update-repo.sh
+++ scripts/update-repo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 IFS=$'\n'
 
-for REPO in `ls "/home/data/NDClab/scripts/"`
+echo "Checking repos in tools"
+for REPO in `ls "/home/data/NDClab/tools/"`
 do
   echo "Checking $REPO"
-  if [ -e "/home/data/NDClab/scripts/$REPO/.git" ]
+  if [ -e "/home/data/NDClab/tools/$REPO/.git" ]
     then
       cd "/home/data/NDClab/scripts/$REPO"
       git fetch
@@ -12,4 +13,32 @@ do
     else
       echo "Not a git repo. Skipping."
   fi
+done
+
+echo "Checking repos in datasets"
+for REPO in `ls "/home/data/NDClab/datasets"`
+do
+  echo "Checking $REPO"
+  if [ -e "/home/data/NDClab/datasets/$REPO/.git" \
+    then
+      cd "home/data/NDClab/datasets/$REPO"
+      git fetch
+      git pull
+    else
+      echo "Not a git repo. Skipping."
+   fi
+done
+
+echo "Checking repos in analyses"
+for REPO in `ls "/home/data/NDClab/analyses"`
+do
+  echo "Checking $REPO"
+  if [ -e "/home/data/NDClab/analyses/$REPO/.git"`
+    then
+      cd "home/data/NDClab/datasets/$REPO"
+      git fetch
+      git pull
+    else
+      echo "Not a git repo. Skipping."
+   fi
 done
