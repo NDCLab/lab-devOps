636340d6a1c14986772da249289b18cbdcff32db
Farukh Said <saidmf@rpi.edu>
2021-10-05 13:53:51 -0400

    Added groups to print membership

--- scripts/print-membership.sh
+++ scripts/print-membership.sh
@@ -7,11 +7,18 @@ then
     exit 9999 
 fi
 
-WRITE=$(getfacl $1 | awk '/^user:.+:rwx$/{print}')
-READ=$(getfacl $1 | awk '/^user:.+:r-x$/{print}')
+WRITE=$(getfacl $1 | awk '/^user:.+:rwx$/||/^group:.*:rwx$/{print}')
+READ=$(getfacl $1 | awk '/^user:.+:r-x$/||/^group:.*:r-x$/{print}')
 
-echo "Members who can write to folder:"
-echo $WRITE
+printf "\nUsers & groups who can read and write to $1:\n\n"
+for ID in $WRITE
+do
+    echo "$(echo $ID | cut -d':' -f 1-2)"
+done
 
-echo "Members who can read from folder:"
-echo $READ
+printf "\nUsers & groups who can only read from $1:\n\n"
+for ID in $READ
+do
+    echo "$(echo $ID | cut -d':' -f 1-2)"
+done
+printf "\n"
