a47f76034d2e0e5cdd60d4e1c1d112dd931f941e
Farukh Said <saidmf@rpi.edu>
2021-10-05 10:47:31 -0400

    created script to automatically clear log files

---
+++ scripts/clean-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A script to clean up log files if they surpass a size. Will be run automatically via cron
+
+val=$(du -shk $1 | awk '{print $1}')
+declare -i LIMIT=5000000
+
+if [ "$val" -gt "$LIMIT" ]
+  then
+    echo "Log file disk usage is above $LIMIT. Clearing $1"
+    echo "Deleting the following:"
+    find $1 -name "*.log" -type f
+    find $1 -name "*.log" -type f -delete
+  else
+    echo "Log file disk usage below $LIMIT. Skipping."
+fi
