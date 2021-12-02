3a809a044aa0831b2f48cf243afb25a3398e89d6
F-said <f.saidmuratov@yahoo.com>
2021-12-02 18:00:11 -0500

    Added check to skip inaccessible dirs

--- scripts/verify-encryption.sh
+++ scripts/verify-encryption.sh
@@ -8,6 +8,10 @@ for DIR in `ls $DATA_PATH`
 do
     if [ -e "$DATA_PATH/$DIR/$ZOOM_PATH" ]; then
         echo "Validating $DIR encryption"
+        if [ "cd "$DATA_PATH/$DIR/$ZOOM_PATH"" grep -q 'Permission denied' ]; then
+            echo "$DIR is not accessible via your permissions" 
+            continue
+        fi
         cd "$DATA_PATH/$DIR/$ZOOM_PATH"
         for SUB in *; do
             echo "checking if contents of $SUB are encrypted"
