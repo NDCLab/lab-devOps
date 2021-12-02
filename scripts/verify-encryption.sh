5887dbc89ce33a87b2e3faf60ff0c98dc45c0ecd
F-said <f.saidmuratov@yahoo.com>
2021-12-02 17:38:11 -0500

    Replaced ls with *

--- scripts/verify-encryption.sh
+++ scripts/verify-encryption.sh
@@ -7,12 +7,10 @@ do
     if [ -e "/home/data/NDClab/datasets/$dir/sourcedata/raw/zoom/" ]; then
         echo "Validating $dir encryption"
         cd "/home/data/NDClab/datasets/$dir/sourcedata/raw/zoom/"
-        for sub in `ls ./`
-        do
-            echo "checking encrypted files of $sub"
+        for sub in `ls ./`; do
+            echo "checking if files of $sub are encrypted"
             cd "$sub"
-            for file in 'ls ./'
-            do
+            for file in *; do
                 if "gpg --list-only $file" grep -q 'gpg: encrypted with \. passphrase'; then
                     echo "file $file encrypted"
                 else
