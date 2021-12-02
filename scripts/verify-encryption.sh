b1067bed0a863f3e7748efe4d8d9e938c5df606c
F-said <f.saidmuratov@yahoo.com>
2021-12-02 17:19:47 -0500

    Fixed list dir command in ls

--- scripts/verify-encryption.sh
+++ scripts/verify-encryption.sh
@@ -7,17 +7,17 @@ do
   echo "Validating $dir encryption"
     if [ -e "/home/data/NDClab/datasets/$dir/sourcedata/raw/zoom/" ]; then
         cd "/home/data/NDClab/datasets/$dir/sourcedata/raw/zoom/"
-        for sub in `ls`
+        for sub in `ls .`
         do
             cd "$sub"
-            for file in 'ls'
+            for file in 'ls .'
             do
                 if "gpg --list-only $file" grep -q 'gpg: encrypted with \. passphrase'; then
                     echo "file $file encrypted"
                 else
                     echo "file $file not encrypted, notifying tech"
-                    loc = $(pwd)
-                    echo "$file failed encryption-check in ${OUTPUT}" | mail -s "Encrypt validation failed" fsaidmur@fiu.edu
+                    LOC = $(pwd)
+                    echo "$file failed encryption-check in ${LOC}" | mail -s "Encrypt validation failed" fsaidmur@fiu.edu
                 fi
             done
         done
