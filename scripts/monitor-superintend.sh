ef46fdd30d26ab4e61e1f69197ec6e94bde55d7e
F-said <f.saidmuratov@yahoo.com>
2022-03-23 16:47:29 -0400

    Updated monitor_superintend to work w/datalad

--- scripts/monitor-superintend.sh
+++ scripts/monitor-superintend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 IFS=$'\n'
-DATA_PATH="/home/data/NDClab/datasets"
-DATA_MONIT="data-monitoring"
+data_path="/home/data/NDClab/datasets"
+monitor_path="data-monitoring"
+monitor_file="data-monitor.sh"
 source_data="sourcedata"
-MONIT_FILE="data-monitor.sh"
-TOOL_PATH="/home/data/NDClab/tools/lab-devOps/scripts"
+tool_path="/home/data/NDClab/tools/lab-devOps/scripts/config-leads.json"
 
 function verify_lead
 {
@@ -20,11 +20,11 @@ function verify_lead
 
 # Run through each dataset available, checking for data monitoring script
 echo "Checking datasets"
-for DIR in `ls $DATA_PATH`
+for dir in `ls $data_path`
 do
-    if [ -e "$DATA_PATH/$DIR/$DATA_MONIT_PATH/$MONIT_FILE" ]; then
-        echo "Monitoring file present in $DIR"
-        cd "$DATA_PATH/$DIR/$DATA_MONIT_PATH"
+    if [ -e "$data_path/$dir/$monitor_path/$monitor_file" ]; then
+        echo "Monitoring file present in $dir"
+        cd "$data_path/$dir/$monitor_path"
 
         # check data provenance status via conda datalad
         STATUS_MSG=$(conda run -n base datalad status 2>&1)
@@ -36,7 +36,7 @@ do
         fi
 
         # continue by extracting and verifying project lead
-        PROJ_LEAD=$(grep -oP "\"$DIR\":\K.*" $TOOL_PATH/config-leads.json | tr -d '"",'| xargs)
+        PROJ_LEAD=$(grep -oP "\"$dir\":\K.*" $tool_path | tr -d '"",'| xargs)
         ver_result=$(verify_lead $PROJ_LEAD)
         res=$?
         if [ $res == 1 ]; then
@@ -55,14 +55,15 @@ do
                       please execute 'datalad save'\\n $item" | mail -s "Modified or Deleted Data in $DIR" "$email"
             elif [[ "$item" =~ "untracked:" ]]; then 
                 # if untracked start data-monitoring process
-                monitor_result=$(sh $DATA_PATH/$DIR/$DATA_MONIT_PATH/$MONIT_FILE)
+                monitor_result=$(./$monitor_file)
                 exit_code=$?
                 if [[ $exit_code == 9999 ]]; then
                     echo "emailing $DIR:$email"
                     echo "$DATA_PATH/$DIR/$source_data $monitor_result" | mail -s "Data Monitoring Failed $DIR" "$email"
+                else
+                    echo "emailing $DIR:$email on monitor success"
+                    echo "$DATA_PATH/$DIR/$source_data $monitor_result" | mail -s "Data Monitoring Succeeded $DIR" "$email"
                 fi
-                echo "emailing $DIR:$email on monitor success"
-                echo "$DATA_PATH/$DIR/$source_data $monitor_result" | mail -s "Data Monitoring Succeeded $DIR" "$email"
             else
                 echo "Unknown changes to dataset. Notifying tech."
             fi
