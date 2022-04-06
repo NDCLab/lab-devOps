6371b97bcd49e525cf10dcb2d0ae18715e573e64
Farukh Said <saidmf@rpi.edu>
2022-04-06 15:11:36 -0400

    Updated rrun to work

--- scripts/rrun.sh
+++ scripts/rrun.sh
@@ -4,11 +4,10 @@
 # USAGE: sh /home/data/NDClab/tools/lab-devOps/scripts/rrun.sh <file_name>.R
 usage() { echo "Usage: sh rrun.sh <file_name>.R" 1>&2; exit 1; }
 
-file_name=$2
+file_name=$1
 
-if [ ! -f $2 || "$file_name" == "*.R" ] 
-then
-    echo "File $2 does not exist or is not an R file." 
+if [[ ! -f "$file_name" || $file_name =~ *.R ]]; then
+    echo "File $file_name does not exist or is not an R file." 
     exit 9999 
 fi
 
@@ -20,7 +19,7 @@ echo -e  "#!/bin/bash\\n
 #SBATCH --ntasks=1\\n
 #SBATCH --time=01:00:00\\n
 module load r-4.0.2-gcc-8.2.0-tf4pnwr\\n
-Rscript ${file_name}.R" >| $sub_file
+Rscript ${file_name}" >| $sub_file
 
 # Submit sub file
 echo "Submitting $sub_file as job"
