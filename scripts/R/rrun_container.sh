70dd528deadcf146d30464f61fb93d0bb199c296
F-said <saidmf@rpi.edu>
2022-06-07 15:39:14 -0400

    Added backup script

---
+++ scripts/R/rrun_container.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# A script to generate a job and submit for some R script
+
+# USAGE: sh /home/data/NDClab/tools/lab-devOps/scripts/rrun.sh <file_name>.R
+usage() { echo "Usage: sh rrun.sh <file_name>.R" 1>&2; exit 1; }
+
+file_name=$1
+
+if [[ ! -f "$file_name" || $file_name =~ *.R ]]; then
+    echo "File $file_name does not exist or is not an R file." 
+    exit 9999 
+fi
+
+# Generate sub file
+sub_file="${file_name}.sub"
+
+echo -e  "#!/bin/bash\\n
+#SBATCH --nodes=1\\n
+#SBATCH --ntasks=1\\n
+#SBATCH --time=01:00:00\\n
+module load r-4.0.2-gcc-8.2.0-tf4pnwr\\n
+singularity exec /home/data/NDClab/tools/containers/R-4.1.2/R-con.simg Rscript ${file_name}" >| $sub_file
+
+# Submit sub file
+echo "Submitting $sub_file as job"
+sbatch $sub_file
+
+# Give confirmation message and instructions
+echo -e "Job submitted. To rerun again, execute \\'sbatch $sub_file \\'"
---
+++ scripts/backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+IFS=$'\n'
+dpath="/home/data/NDClab/datasets"
+backpath="/home/data/NDClab/other/backups"
+backup_list=("sourcedata" "derivatives")
+
+for dir in `ls $dpath`
+do
+  if [[ "$dir" != "test-readAloud-v3" ]]; then
+      continue
+  fi
+  echo "Checking $dir"
+  if [ ! -d "$backpath/$dir" ]; then
+    mkdir $backpath/$dir
+  fi 
+
+  for backup in "${backup_list[@]}" 
+  do
+    if [ ! -d "$backpath/$dir/$backup" ]; then
+      cd $dpath/$dir/$backup && find . -type d -exec mkdir -p -- $backpath/$dir/$backup{} \;
+    fi
+    for file in $(find "$dpath/$dir/$backup" -type f);
+    do
+      # extract path and filename
+      orig_path=$(dirname $file)
+      filename=$(basename $file)
+
+      # strip original path
+      backpath_name="${backup}."
+      path=$(echo $orig_path | cut -d'/' -f8-)
+      echo $backpath/$dir/$backpath_name/$path/$filename
+      # create hardlink
+      ln $orig_path/$filename $backpath/$dir/$backpath_name/$path/"${filename}-link-`date +"%d-%m-%Y"`"
+    done
+  done
+done
