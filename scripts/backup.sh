2d548aef1af157f3f4c649e5602995fdf16bc065
F-said <saidmf@rpi.edu>
2022-06-10 13:47:03 -0400

    Released the script. Removed the gate.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -25,9 +25,33 @@ do
       # strip original path
       backpath_name="${backup}."
       path=$(echo $orig_path | cut -d'/' -f8-)
-      echo $backpath/$dir/$backpath_name/$path/$filename
-      # create hardlink
-      ln $orig_path/$filename $backpath/$dir/$backpath_name/$path/"${filename}-link-`date +"%d-%m-%Y"`"
+      echo "checking file $backpath/$dir/$backpath_name/$path/$filename"
+
+      # create hardlink using date and filename
+      name="${filename}-link-"
+      date_name="${name}`date +"%m-%d-%Y"`"
+   
+      echo "creating link $date_name"
+
+      # check if last known link to file exists already
+      linked_files=$(ls $backpath/$dir/$backpath_name/$path/$name* 2>/dev/null || echo "empty")
+      if [[ $linked_files == "empty" ]]; then
+          ln $orig_path/$filename $backpath/$dir/$backpath_name/$path/$date_name
+          continue
+      fi 
+      for i in "${!linked_files[@]}"; do
+          file_ln="${linked_files[$i]}"
+          echo "checking difference of $file_ln"
+          # check if it matches the contents of the original file.
+          res=$(cmp --silent $file_ln $orig_path/$filename || echo "true")
+          if [[ "$res" == "true" ]]; then
+              echo "File exists already, skipping"
+              break
+          fi
+      done
+      if [[ "$res" != "true" ]]; then
+          ln $orig_path/$filename $backpath/$dir/$backpath_name/$path/$date_name
+      fi
     done
   done
 done
