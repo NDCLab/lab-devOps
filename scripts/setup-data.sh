7166b7aaf3e7648a880263ddf96ceeb7bcbfe1fd
F-said <f.saidmuratov@yahoo.com>
2022-07-06 15:31:53 -0400

    specified lab devops path

--- scripts/setup-data.sh
+++ scripts/setup-data.sh
@@ -6,15 +6,16 @@ usage() { echo "Usage: setup-data.sh <project-path>" 1>&2; exit 1; }
 project=$1
 datam_path="/data-monitoring"
 code_path="/code"
+labpath = "/home/data/NDClab/tools/lab-devOps/scripts/"
 
 echo "Setting up hallMonitor helper files"
-cp ./monitor-template/rename-cols.py $project
-cp ./monitor-template/update-tracker.py $project
-cp ./monitor-template/README.md $project
+cp "${labpath}/monitor-template/rename-cols.py" "${project}/${datam_path}"
+cp "${labpath}/monitor-template/update-tracker.py" "${project}/${datam_path}"
+cp "${labpath}/monitor-template/README.md" "${project}/${datam_path}"
 echo "Setting up hallMonitor.sh"
-cp ./monitor-template/hallMonitor.sh $project
+cp "${labpath}/monitor-template/hallMonitor.sh" "${project}/${datam_path}"
 echo "Setting up hallMonitor.sub"
-cp ./monitor-template/hallMonitor.sub $project
+cp "${labpath}/monitor-template/hallMonitor.sub" "${project}/${datam_path}"
 
 echo "Setting up preprocess.sub"
-cp ./monitor-template/preprocess.sub $project
+cp "${labpath}/monitor-template/preprocess.sub" "${project}/${datam_path}"
