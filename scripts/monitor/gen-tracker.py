176cc440e28af0cd2546056a14decd7e44294d2d
F-said <f.saidmuratov@yahoo.com>
2022-08-25 15:26:02 -0500

    added incrementor

--- scripts/monitor/gen-tracker.py
+++ scripts/monitor/gen-tracker.py
@@ -2,11 +2,12 @@ import sys
 
 DEF_COLS = ("id", "consent")
 DATA_LAB = "Data"
+INCREMENT = 20
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
     datatypes = sys.argv[2]
-    ids = sys.argv[3]
+    ids = int(sys.argv[3])
 
     # list and label the available datatypes
     header = list(DEF_COLS) + [dt + DATA_LAB for dt in datatypes.split(",")]
@@ -14,3 +15,6 @@ if __name__ == "__main__":
     with open(filepath, "w") as file:
         # write columns
         file.write(','.join(header))
+        for i in range(INCREMENT):
+            file.write(ids)
+            ids += 1
