81a91c381f13f1a4d05820d03da14137c206c4f6
F-said <f.saidmuratov@yahoo.com>
2022-09-22 13:56:45 -0400

    Updated gen-tracker to track first 1000 parts

--- scripts/monitor/gen-tracker.py
+++ scripts/monitor/gen-tracker.py
@@ -5,15 +5,19 @@ SUB_NUM = 1000
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
-    id = sys.argv[2]
+    id_stand = int(sys.argv[2]) 
 
+    # take note of headers
     headers = []
     with open(DATA_DICT) as dd:
+        # skip header
+        next(dd)
         for row in dd:
             headers.append(row.replace(',', '|', 1).split("|")[0])
 
-    # include arbitrary number of parts
-
+    # write id 1000 numbers 
     with open(filepath, "w") as file:
         # write columns
         file.write(','.join(headers) + "\n")
+        for id in range(id_stand, id_stand + SUB_NUM):
+            file.write(str(id) + "\n")
