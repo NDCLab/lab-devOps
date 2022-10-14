264a220beafd998a2c48136934d6027f0fd1918a
F-said <f.saidmuratov@yahoo.com>
2022-10-14 15:21:46 -0400

    Added remove script

---
+++ scripts/compl/remove.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# A script to remove id from all datasets & analysis
+
+# USAGE: make-private </data/path>
+usage() { echo "Usage: sh remove.sh <id>" 1>&2; exit 1; }
+
+id=$1
+project=$2
+
+dpath="/home/data/NDClab/datasets"
+apath="/home/data/NDClab/analyses"
+
+for dset in `ls $dpath`
+do
+    setfacl -Rx u:$id,d:u:$id $dset
+done
+
+for aset in `ls $apath`
+do
+    setfacl -Rx u:$id,d:u:$id $aset
+done
+
+
+echo "Member" 
