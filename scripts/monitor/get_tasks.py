4084ec2462d222f45ac50956f5220c3c972254c8
F-said <f.saidmuratov@yahoo.com>
2022-10-26 16:04:44 -0400

    Added proper variable

--- scripts/monitor/get_tasks.py
+++ scripts/monitor/get_tasks.py
@@ -5,13 +5,10 @@ if __name__ == "__main__":
     filepath = sys.argv[1]
 
     tasks = ""
-
-    # take note of headers
-    headers = []
     with open(filepath, "r") as dd:
         reader = csv.DictReader(dd)
         for row in reader:
             if "task status" in row["description"]:
-                tasks += row["variable"] + ","
+                tasks += row["\xef\xbb\xbfvariable"] + ","
 
     print(tasks[:-1])
