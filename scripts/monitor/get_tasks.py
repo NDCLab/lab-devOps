79c9aef5f70796b8f72156d1b5eec975d5ebdd51
F-said <f.saidmuratov@yahoo.com>
2022-10-26 15:58:40 -0400

    Created get_tasks

---
+++ scripts/monitor/get_tasks.py
@@ -0,0 +1,16 @@
+import csv 
+
+if __name__ == "__main__":
+    filepath = sys.argv[1]
+
+    tasks = ""
+
+    # take note of headers
+    headers = []
+    with open(filepath) as dd:
+        reader = csv.DictReader(dd)
+        for row in dd:
+            if "task status" in row["description"]:
+                tasks += row["variable"] + ","
+
+    print(tasks[:-1])
