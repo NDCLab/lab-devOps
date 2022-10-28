e7bf418efd747dc5bf53e96dfa5269961b5be55d
F-said <f.saidmuratov@yahoo.com>
2022-10-28 16:55:51 -0400

    Modded remove

--- scripts/compl/remove.sh
+++ scripts/compl/remove.sh
@@ -10,12 +10,10 @@ project=$2
 dpath="/home/data/NDClab/datasets"
 apath="/home/data/NDClab/analyses"
 
-for dset in `ls $dpath`
-do
+for dset in `ls $dpath`; do
     setfacl -Rx u:$id,d:u:$id "${dpath}/${dset}"
 done
 
-for aset in `ls $apath`
-do
+for aset in `ls $apath`; do
     setfacl -Rx u:$id,d:u:$id "${apath}/${aset}"
 done
