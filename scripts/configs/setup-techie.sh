598bd07cb04f49e77a2307e1effbabddb60f4e53
F-said <f.saidmuratov@yahoo.com>
2022-11-03 14:56:13 -0400

    Modded setup

--- scripts/configs/setup-techie.sh
+++ scripts/configs/setup-techie.sh
@@ -8,21 +8,21 @@
 # verify-encrpytion: verify that all sensitive data files are encrypted
 # check-hpc-status: check if cpu usage is too high
 # backup: backup all data folders by creating softlinks
-crontab -l | { cat; echo "0 7 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/update-repo.sh > /home/data/NDClab/other/logs/repo-updates/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "0 7 1 1 * /home/data/NDClab/tools/lab-devOps/scripts/backup/clean-logs.sh /home/data/NDClab/other/logs > /home/data/NDClab/other/logs/clean-up/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "0 7 3 7 * /home/data/NDClab/tools/lab-devOps/scripts/backup/clean-logs.sh /home/data/NDClab/other/logs > /home/data/NDClab/other/logs/clean-up/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "0 7 * * * /home/data/NDClab/tools/lab-devOps/scripts/compl/verify-encryption.sh > /home/data/NDClab/other/logs/encrypt-checks/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 9 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 10 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 11 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 12 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 13 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "30 14 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 15 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 16 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "59 17 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "0 0 * * 0 /home/data/NDClab/tools/lab-devOps/scripts/backup/backup.sh > /home/data/NDClab/other/logs/backups/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
-crontab -l | { cat; echo "0 0 * * 3 /home/data/NDClab/tools/lab-devOps/scripts/backup/backup.sh > /home/data/NDClab/other/logs/backups/"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 7 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/update-repo.sh > /home/data/NDClab/other/logs/repo-updates/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 7 1 1 * /home/data/NDClab/tools/lab-devOps/scripts/backup/clean-logs.sh /home/data/NDClab/other/logs > /home/data/NDClab/other/logs/clean-up/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 7 3 7 * /home/data/NDClab/tools/lab-devOps/scripts/backup/clean-logs.sh /home/data/NDClab/other/logs > /home/data/NDClab/other/logs/clean-up/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 7 * * * /home/data/NDClab/tools/lab-devOps/scripts/compl/verify-encryption.sh > /home/data/NDClab/other/logs/encrypt-checks/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 9 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 10 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 11 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 12 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 13 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 14 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 15 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 16 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/\"`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"`\".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "59 17 * * * /home/data/NDClab/tools/lab-devOps/scripts/updates/check-hpc-status.sh > /home/data/NDClab/other/logs/cpu-history/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 0 * * 0 /home/data/NDClab/tools/lab-devOps/scripts/backup/backup.sh > /home/data/NDClab/other/logs/backups/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
+crontab -l | { cat; echo "0 0 * * 3 /home/data/NDClab/tools/lab-devOps/scripts/backup/backup.sh > /home/data/NDClab/other/logs/backups/"\`date +"\\%m_\\%d_\\%Y::\\%H:\\%M:\\%S"\`".log 2>&1"; } | crontab -
 
 # add umask
 
