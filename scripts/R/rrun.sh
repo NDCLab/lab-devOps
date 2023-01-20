00ba6c4c24c67fa5156ba770c688373cc9626d42
David Hunt <davhunt@iu.edu>
2023-01-20 13:28:20 -0500

    update HPC scripts
    make node # and walltime configurable for python, R, matlab script submissions to HPC

--- scripts/R/rrun.sh
+++ scripts/R/rrun.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 # A script to generate a job and submit for some R script
 
-# USAGE: sh /home/data/NDClab/tools/lab-devOps/scripts/rrun.sh <file_name>.R
-usage() { echo "Usage: sh rrun.sh <file_name>.R" 1>&2; exit 1; }
+# USAGE: sh /home/data/NDClab/tools/lab-devOps/scripts/rrun.sh <file_name>
+usage() { echo "Usage: sh rrun.sh <file_name> [# nodes] [walltime (in hours)]" 1>&2; exit 1; }
+
+[[ $# -eq 0 ]] && usage
 
 file_name=$1
+# use 1 node, 1 hr walltime by default
+NODES=${2:-"1"}
+WALLTIME="${3:-"1"}:00:00"
 
-if [[ ! -f "$file_name" || $file_name =~ *.R ]]; then
-    echo "File $file_name does not exist or is not an R file." 
-    exit 9999 
+if [[ "${file_name##*.}" != "R" ]]; then file_name="${file_name}".R; fi
+
+if [[ ! -f "$file_name" ]]; then
+    echo "File $file_name does not exist."
+    exit 9999
 fi
 
 # Generate sub file
-sub_file="${file_name}.sub"
+sub_file="${file_name%.*}.sub"
+
+R_MODULE="r-4.2.0-gcc-8.2.0"
 
 echo -e  "#!/bin/bash\\n
-#SBATCH --nodes=1\\n
+#SBATCH --nodes=$NODES\\n
 #SBATCH --ntasks=1\\n
-#SBATCH --time=01:00:00\\n
-module load r-4.0.2-gcc-8.2.0-tf4pnwr\\n
+#SBATCH --time=$WALLTIME\\n
+module load $R_MODULE\\n
 Rscript ${file_name}" >| $sub_file
 
 # Submit sub file
--- scripts/R/rrun_container.sh
+++ scripts/R/rrun_container.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 # A script to generate a job and submit for some R script
 
-# USAGE: sh /home/data/NDClab/tools/lab-devOps/scripts/rrun.sh <file_name>.R
-usage() { echo "Usage: sh rrun.sh <file_name>.R" 1>&2; exit 1; }
+# USAGE: sh /home/data/NDClab/tools/lab-devOps/scripts/rrun.sh <file_name>
+usage() { echo "Usage: sh rrun.sh <file_name> [# nodes] [walltime (in hours)]" 1>&2; exit 1; }
+
+[[ $# -eq 0 ]] && usage
 
 file_name=$1
+# use 1 node, 1 hr walltime by default
+NODES=${2:-"1"}
+WALLTIME="${3:-"1"}:00:00"
 
-if [[ ! -f "$file_name" || $file_name =~ *.R ]]; then
-    echo "File $file_name does not exist or is not an R file." 
-    exit 9999 
+if [[ "${file_name##*.}" != "R" ]]; then file_name="${file_name}".R; fi
+
+if [[ ! -f "$file_name" ]]; then
+    echo "File $file_name does not exist."
+    exit 9999
 fi
 
 # Generate sub file
-sub_file="${file_name}.sub"
+sub_file="${file_name%.*}.sub"
+
+SINGULARITY_MODULE="singularity-3.8.2"
 
 echo -e  "#!/bin/bash\\n
-#SBATCH --nodes=1\\n
+#SBATCH --nodes=$NODES\\n
 #SBATCH --ntasks=1\\n
-#SBATCH --time=01:00:00\\n
-module load r-4.0.2-gcc-8.2.0-tf4pnwr\\n
-singularity exec /home/data/NDClab/tools/containers/R-4.1.2/R-con.simg Rscript ${file_name}" >| $sub_file
+#SBATCH --time=$WALLTIME\\n
+module load $SINGULARITY_MODULE\\n
+singularity exec /home/data/NDClab/tools/containers/R-4.2.2A/R-4.2.2A.simg Rscript ${file_name}" >| $sub_file
 
 # Submit sub file
 echo "Submitting $sub_file as job"
