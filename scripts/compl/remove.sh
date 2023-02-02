9b59656f3761aa51a61f2cbee43388f7f825f1bf
David Hunt <davhunt@iu.edu>
2023-02-02 13:48:49 -0500

    add tools to remove.sh script

--- scripts/compl/remove.sh
+++ scripts/compl/remove.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 # A script to remove id from all datasets & analysis
 
-# USAGE: make-private </data/path>
-usage() { echo "Usage: sh remove.sh <id>" 1>&2; exit 1; }
+# USAGE: remove <id>
+usage() { echo "Usage: bash remove.sh <id>"; exit 0; }
+
+if [[ $# -ne 1 ]]; then usage; fi
 
 id=$1
-project=$2
 
 dpath="/home/data/NDClab/datasets"
 apath="/home/data/NDClab/analyses"
+tpath="/home/data/NDClab/tools"
 
-for dset in `ls $dpath`; do
-    setfacl -Rx u:$id,d:u:$id "${dpath}/${dset}" 2> /dev/null
-done
-
-for aset in `ls $apath`; do
-    setfacl -Rx u:$id,d:u:$id "${apath}/${aset}" 2> /dev/null
+for dir in $dpath $apath $tpath; do
+  for repo in `ls $dir`; do
+    echo "removing $id from $repo"
+    setfacl -Rx u:$id,d:u:$id "${dir}/${repo}"
+  done
 done
