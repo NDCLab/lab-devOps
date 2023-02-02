02e793b64cb00d9cdd399d8eed37d8112cbddc9c
David Hunt <davhunt@iu.edu>
2023-02-02 13:56:37 -0500

    add new-project-lead, onboard-new, aud script
    new-project-lead.sh grants full read/write access to a project for a new proj lead; onboard-new grants read access to lab repos (except sourcedata and derivatives); print-permissions.sh prints acl info for a list of users or projects

---
+++ scripts/compl/onboard-new.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# A script to grant read access to all lab project repos to a new team member.
+
+# USAGE: bash onboard-new.sh <user> 
+
+usage() { echo "Usage: bash $0 <user1>"; exit 0; }
+
+if [[ $# -eq 0 ]]; then usage; fi
+
+ID=$1
+DATA_PATH=/home/data/NDClab/datasets
+TOOL_PATH=/home/data/NDClab/tools
+ANA_PATH=/home/data/NDClab/analyses
+
+for dir in $DATA_PATH $TOOL_PATH $ANA_PATH
+  do
+  for repo in $(ls $dir)
+    do
+    setfacl -Rm d:u:$ID:r--,u:$ID:r-- $dir/$repo
+    if [[ $dir == "$DATA_PATH" ]]; then
+      setfacl -Rm d:u:$ID:---,u:$ID:--- $dir/$repo/sourcedata
+      setfacl -Rm d:u:$ID:---,u:$ID:--- $dir/$repo/derivatives
+      # don't grant access to sourcedata and derivatives
+    fi
+  done
+done
