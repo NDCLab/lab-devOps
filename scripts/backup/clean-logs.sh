4422cf246c0076618e017bd959a3dca07e1554ba
David Hunt <davhunt@iu.edu>
2023-02-02 14:02:26 -0500

    delete year-old logs

--- scripts/backup/clean-logs.sh
+++ scripts/backup/clean-logs.sh
@@ -6,7 +6,7 @@ usage() { echo "Usage: bash $0"; exit 0; }
 log_dir="/home/data/NDClab/other/logs"
 
 # delete logs older than six months
-older_than=`date -d "-6 month" +%y-%m`
+older_than=`date -d "-12 month" +%y-%m`
 find "$log_dir" -name "*.log" -exec bash -c \
   'timestamp=$(basename $1) && if [[ "${timestamp:8:2}"-"${timestamp:0:2}" < $2 ]]; \
   then echo "deleting $1" && rm $1 2>&1; fi' bash {} $older_than ';'
