a57333682496c0ec85ed2b7bf64643983bb1fd42
David Hunt <davhunt@iu.edu>
2023-05-01 13:47:15 -0400

    skip over archived repos

--- scripts/updates/update-repo.sh
+++ scripts/updates/update-repo.sh
@@ -3,46 +3,59 @@ IFS=$'\n'
 TOOL_PATH="/home/data/NDClab/tools"
 DATA_PATH="/home/data/NDClab/datasets"
 ANA_PATH="/home/data/NDClab/analyses"
+ARCHIVED_REPOS="rwe-dataset social-context-dataset social-context-beta-dataset social-context-gamma-dataset autism-go-academy missing-link-dataset mind-reading"
 
 echo "Checking repos in tools"
 for REPO in `ls $TOOL_PATH`
 do
-  echo "Checking $REPO"
-  if [ -e "$TOOL_PATH/$REPO/.git" ]
-    then
-      cd "$TOOL_PATH/$REPO"
-      git fetch
-      git pull
-    else
-      echo "Not a git repo. Skipping."
+  if [[ $ARCHIVED_REPOS != *"$REPO"* ]]; then
+    echo "Checking $REPO"
+    if [ -e "$TOOL_PATH/$REPO/.git" ]
+      then
+        cd "$TOOL_PATH/$REPO"
+        git fetch
+        git pull
+      else
+        echo "Not a git repo. Skipping."
+    fi
+  else
+    echo "Skipping archived repo $REPO"
   fi
 done
 
 echo "Checking repos in datasets"
 for REPO in `ls $DATA_PATH`
 do
-  echo "Checking $REPO"
-  if [ -e "$DATA_PATH/$REPO/.git" ]
-    then
-      cd "$DATA_PATH/$REPO"
-      git fetch
-      git pull
+  if [[ $ARCHIVED_REPOS != *"$REPO"* ]]; then
+    echo "Checking $REPO"
+    if [ -e "$DATA_PATH/$REPO/.git" ]
+      then
+        cd "$DATA_PATH/$REPO"
+        git fetch
+        git pull
     else
-      echo "Not a git repo. Skipping."
-   fi
+        echo "Not a git repo. Skipping."
+    fi
+  else
+    echo "Skipping archived repo $REPO"
+  fi
 done
 
 echo "Checking repos in analyses"
 for REPO in `ls $ANA_PATH`
 do
-  echo "Checking $REPO"
-  if [ -e "$ANA_PATH/$REPO/.git" ]
-    then
-      cd "$ANA_PATH/$REPO"
-      git fetch
-      git pull
+  if [[ $ARCHIVED_REPOS != *"$REPO"* ]]; then
+    echo "Checking $REPO"
+    if [ -e "$ANA_PATH/$REPO/.git" ]
+      then
+        cd "$ANA_PATH/$REPO"
+        git fetch
+        git pull
     else
-      echo "Not a git repo. Skipping."
-   fi
+        echo "Not a git repo. Skipping."
+    fi
+  else
+    echo "Skipping archived repo $REPO"
+  fi
 done
 
