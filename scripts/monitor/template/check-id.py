cc72947d9817be7edecb0825dbe78724d087df7a
David Hunt <davhunt@iu.edu>
2023-08-15 14:42:18 -0400

    check pandas version

--- scripts/monitor/template/check-id.py
+++ scripts/monitor/template/check-id.py
@@ -6,13 +6,17 @@ if __name__ == "__main__":
     file = sys.argv[2]
     
     # extract id col
-    file_df = pd.read_csv(file, on_bad_lines="skip")
+    if pd.__version__ >= "1.4.0":
+        file_df = pd.read_csv(file, on_bad_lines="skip")
+    else:
+        file_df = pd.read_csv(file, error_bad_lines=False, warn_bad_lines=False)
     if "id" in file_df:
         id_col = file_df["id"]
     elif "participant" in file_df:
         id_col = file_df["participant"]
     else:
-        print("False, cannot find id or participant column")
+        print("Error: cannot find id or participant column in", file)
 
     # check if first ids match vals listed
-    print(id_col[0] == int(id))
+    if not id_col[0] == int(id):
+        print("Error: ID value in", file, "does not match", id)
