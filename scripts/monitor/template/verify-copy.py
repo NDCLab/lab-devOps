4556132bdc8dfabe65d0bab20d2ff51e7c4c4341
David Hunt <davhunt@iu.edu>
2023-08-16 12:06:23 -0400

    take allowed subj #s from datadict

--- scripts/monitor/template/verify-copy.py
+++ scripts/monitor/template/verify-copy.py
@@ -8,6 +8,7 @@ import shutil
 import pandas as pd
 import re
 import math
+import subprocess
 
 #print(sys.version)
 
@@ -49,16 +50,20 @@ if __name__ == "__main__":
     for var, row in df_dd.iterrows():
         if row["dataType"] not in datatypes_to_ignore and (isinstance(row["dataType"], str) or not math.isnan(row["dataType"])):
             #dd_dict[row["variable"]] = [row["dataType"], row["allowedSuffix"], row["expectedFileExts"]]
-            dd_dict[var] = [row["dataType"], row["allowedSuffix"], row["expectedFileExts"]]
+            dd_dict[var] = [row["dataType"], row["allowedSuffix"], row["expectedFileExts"], row["allowedValues"]]
+
+    allowed_subs = df_dd.loc["id", "allowedValues"]
+    allowed_subs_re = "^(" + allowed_subs.replace(" ", "").replace("X", ".").replace(",", "|") + ")$"
 
     # now search sourcedata/raw for correct files
     for key, values in dd_dict.items():
         print("Verifying files for:", key)
         #presence = False
-        taskname = key
+        variable = key
         datatype = values[0]
         allowed_suffixes = values[1].split(", ")
         fileexts = values[2].split(", ") # with or without . ?
+        allowed_vals = values[3].split(", ")
         possible_exts = sum([ext.split('|') for ext in fileexts], [])
         numfiles = len(fileexts)
 
@@ -85,33 +90,38 @@ if __name__ == "__main__":
                             obs_files.append(raw_file)
                     if corrected:
                         print("Corrected.txt seen in ", join(raw, ses, datatype, subject), ", skipping copy.")
-                        ##############################TODO
+                        for raw_file in listdir(join(raw, ses, datatype, subject)):
+                            # still check that files are in the correct subject folder and session folder
+                            file_re = re.match("^(sub-[0-9]*)_([a-zA-Z0-9_-]*_)?(s[0-9]*_r[0-9]*)_e[0-9]*.*$", raw_file)
+                            if file_re and file_re.group(1) != subject:
+                                print("Error: file from subject", file_re.group(1), "found in", subject, "folder:", join(raw, ses, datatype, subject, raw_file))
+                            if file_re and len(ses) > 0 and file_re.group(3) != ses:
+                                print("Error: file from session", file_re.group(3), "found in", ses, "folder:", join(raw, ses, datatype, subject, raw_file))
                         continue
                     if len(obs_files) > numfiles:
-                        print("Error: number of data files in subject folder", subject, obs_files, "greater than the expected number", numfiles)
+                        print("Error: number of", datatype, "data files in subject folder", subject, len(obs_files), "greater than the expected number", numfiles)
                     elif len(obs_files) < numfiles:
-                        print("Error: number of data files in subject folder", subject, obs_files, "less than the expected number", numfiles)
+                        print("Error: number of", datatype, "data files in subject folder", subject, len(obs_files), "less than the expected number", numfiles)
                     for raw_file in listdir(join(raw, ses, datatype, subject)):
                         #check sub-#, check session folder, check extension
-                        #file_re = re.match("^(sub-[0-9]+)_(" + taskname + ")_(s([0-9]*)_r([0-9]*))_e([0-9]*)((?:\.[a-zA-Z]+)*)$", raw_file)
-                        file_re = re.match("^(sub-([0-9]*))_([a-zA-Z0-9_]*)_((s([0-9]*)_r([0-9]*))_e([0-9]*))((?:\.[a-zA-Z]+)*)$", raw_file)
+                        file_re = re.match("^(sub-([0-9]*))_([a-zA-Z0-9_-]*)_((s([0-9]*)_r([0-9]*))_e([0-9]*))((?:\.[a-zA-Z]+)*)$", raw_file)
                         if file_re:
                             if file_re.group(1) != subject:
                                 print("Error: file from subject", file_re.group(1), "found in", subject, "folder:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(5) != ses and len(ses) > 0:
                                 print("Error: file from session", file_re.group(5), "found in", ses, "folder:", join(raw, ses, datatype, subject, raw_file))
-                            #possible_exts = list(np.concatenate([ext.split('|') for ext in fileexts]).flat)
-                            #possible_exts = sum([ext.split('|') for ext in fileexts], [])
-                            if file_re.group(9) not in possible_exts:
+                            if file_re.group(9) not in possible_exts and len(file_re.group(9)) > 0:
                                 print("Error: file with extension", file_re.group(9), "found, doesn\'t match expected extensions", ", ".join(possible_exts), ":", join(raw, ses, datatype, subject, raw_file))
+                            if not re.match(allowed_subs_re, file_re.group(2)) and file_re.group(2) != '':
+                                print("Error: subject number", file_re.group(2), "not an allowed subject value", allowed_subs, "in file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(3) not in dd_dict.keys():
-                                print("Error: taskname", file_re.group(3), "does not match any datadict tasknames, in file:", join(raw, ses, datatype, subject, raw_file))
+                                print("Error: variable name", file_re.group(3), "does not match any datadict variables, in file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(4) not in allowed_suffixes:
                                 print("Error: suffix", file_re.group(4), "not in allowed suffixes", ", ".join(allowed_suffixes), "in file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(2) == "":
                                 print("Error: subject # missing from file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(3) == "":
-                                print("Error: taskname missing from file:", join(raw, ses, datatype, subject, raw_file))
+                                print("Error: variable name missing from file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(6) == "":
                                 print("Error: session # missing from file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(7) == "":
@@ -120,9 +130,13 @@ if __name__ == "__main__":
                                 print("Error: event # missing from file:", join(raw, ses, datatype, subject, raw_file))
                             if file_re.group(9) == "":
                                 print("Error: extension missing from file:", join(raw, ses, datatype, subject, raw_file))
-
+                            if datatype == "psychopy" and file_re.group(9) == ".csv" and file_re.group(2) != "":
+                                # Call check-id.py for psychopy files
+                                subprocess.run(["python3", "check-id.py", file_re.group(2), join(raw, ses, datatype, subject, raw_file)], shell=False)
                         else:
-                            print("Error: file ", raw_file, " does not match naming convention <sub-#>_<task-name>_<session>.<ext>")
+                            print("Error: file ", join(raw, ses, datatype, subject, raw_file), " does not match naming convention <sub-#>_<variable/task-name>_<session>.<ext>")
+
+
 
 
 
@@ -141,7 +155,7 @@ if __name__ == "__main__":
                                     # wrong session folder, wrong subject folder
 
 
-                                    if re.match(subject + "_" + taskname + "_" + suffix + ext, raw_file):
+                                    if re.match(subject + "_" + variable + "_" + suffix + ext, raw_file):
                                     #if re.match(subject + "_" + taskname + "_" + suffix + "." + ext, raw_file):
                                         presence = True
                                         # copy file to checked, unless "corrected" is seen
@@ -164,7 +178,8 @@ if __name__ == "__main__":
                             obs_exts = {file: "."+".".join(file.split('.')[1:]) for file in copied_files} #".zip.gpg" ?
                             for ext in fileexts:
                                 if not ext in list(obs_exts.values()):
-                                    print("No ", subject + "_" + taskname + "_" + suffix + ext, " file found in ", join(raw, ses, datatype, subject))
+                                    pass
+                                    #print("No ", subject + "_" + taskname + "_" + suffix + ext, " file found in ", join(raw, ses, datatype, subject))
                             #if not presence:
                             #    print("Can\'t find: ", subject + "_" + taskname + "_" + suffix + ext, " file in ", join(raw, ses, datatype, subject))
             else:
