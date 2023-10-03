25d028ff0a08bddf26dc3715b8fdf4f27c17ff33
David Hunt <davhunt@iu.edu>
2023-10-03 16:07:11 -0400

    check for issues in datadict, duplicate columns etc. before gen-tracker

--- scripts/monitor/gen-tracker.py
+++ scripts/monitor/gen-tracker.py
@@ -1,17 +1,53 @@
 import sys
 import pandas as pd
 from os.path import basename
+import collections
+
+sessionless = ["id", "consent", "assent"]
+
+def check_data_dict_variables(df_dd):
+    all_tracker_cols = []
+    for _, row in df_dd.iterrows():
+        if row["variable"] not in sessionless:
+            for suf in row["allowedSuffix"].split(', '):
+                all_tracker_cols.append(row["variable"] + "_" + suf)
+    duplicates = [col for col, count in collections.Counter(all_tracker_cols).items() if count > 1]
+    if len(duplicates) > 0:
+        sys.exit("Error in data dictionary, duplicate column names seen: " + ", ".join(duplicates))
+
+def check_data_dict_provenance(df_dd):
+    all_redcap_cols = []
+    for _, row in df_dd.iterrows():
+        if row["dataType"] == "redcap_data":
+            prov = row["provenance"].split(" ")
+            for i in prov:
+                if "file:" in i:
+                    idx = prov.index(i)+1
+                    redcap_file = prov[idx].strip("\"\';,()")
+                elif "variable:" in i:
+                    idx = prov.index(i)+1
+                    redcap_var = prov[idx].strip("\"\';,()")
+                    if redcap_var == "":
+                        redcap_var = row["variable"]
+            for suf in row["allowedSuffix"].split(', '):
+                all_redcap_cols.append("redcap: " + redcap_file + ", variable: " + redcap_var + "_" + suf)
+    duplicates = [col for col, count in collections.Counter(all_redcap_cols).items() if count > 1]
+    if len(duplicates) > 0:
+        sys.exit("Error in data dictionary, duplicate provenances seen: " + "; ".join(duplicates))
+
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
     project = sys.argv[2]
     redcaps = sys.argv[3]
-
+    
     redcaps = redcaps.split(",")
     DATA_DICT = "/home/data/NDClab/datasets/{}/data-monitoring/data-dictionary/central-tracker_datadict.csv".format(project)
     df_dd = pd.read_csv(DATA_DICT)
-
-    id_desc = df_dd.set_index("variable").loc["id", "description"].split(" ")
+    check_data_dict_variables(df_dd)
+    check_data_dict_provenance(df_dd)
+    
+    id_desc = df_dd.set_index("variable").loc["id", "provenance"].split(" ")
     # ID description column should contain redcap and variable from which to read IDs, in format 'file: "{name of redcap}"; variable: "{column name}"'
     for i in id_desc:
         if "file:" in i:
@@ -27,11 +63,10 @@ if __name__ == "__main__":
             consent_redcap = pd.read_csv(redcap, index_col=var)
             break
     if "consent_redcap" not in locals():
-        sys.exit("Can\'t find", id_rc, "redcap to read IDs from")
+        sys.exit("Can\'t find" + id_rc + "redcap to read IDs from")
     ids = consent_redcap.index.tolist()
-
+    
     headers = []
-    sessionless = ["id", "consent", "assent"]
     headers.extend(sessionless)
     for _, row in df_dd.iloc[0:].iterrows():
         # skip header
