dfcbbdc417ecf02c362095dbbed21008d9329e51
David Hunt <davhunt@iu.edu>
2023-10-27 16:27:01 -0400

    check checked files, as well as raw

--- scripts/monitor/template/verify-copy.py
+++ scripts/monitor/template/verify-copy.py
@@ -30,6 +30,65 @@ def allowed_val(allowed_vals, value):
             break
     return allowed
 
+def check_filenames(path, sub, ses, datatype, allowed_suffixes, possible_exts, numfiles):
+        # check that files in raw and checked match conventions
+        obs_files = []
+        corrected = False
+        for raw_file in listdir(path):
+            if re.match('^[Dd]eviation.*$', raw_file):
+                corrected = True
+            else:
+                obs_files.append(raw_file)
+        if corrected:
+            print("deviation.txt seen in ", path, ", continuing.")
+            for raw_file in listdir(path):
+                # still check that files are in the correct subject folder and session folder
+                file_re = re.match("^(sub-[0-9]*)_([a-zA-Z0-9_-]*_)?(s[0-9]*_r[0-9]*)_e[0-9]*.*$", raw_file)
+                if file_re and file_re.group(1) != sub:
+                    print(c.RED + "Error: file from subject", file_re.group(1), "found in", sub, "folder:", path + c.ENDC)
+                if file_re and len(ses) > 0 and file_re.group(3) != ses:
+                    print(c.RED + "Error: file from session", file_re.group(3), "found in", ses, "folder:", path + c.ENDC)
+            return
+        if len(obs_files) > numfiles:
+            print(c.RED + "Error: number of", datatype, "data files in subject folder", sub, str(len(obs_files)), "greater than the expected number", str(numfiles) + c.ENDC)
+        elif len(obs_files) < numfiles:
+            print(c.RED + "Error: number of", datatype, "data files in subject folder", sub, str(len(obs_files)), "less than the expected number", str(numfiles) + c.ENDC)
+        for raw_file in listdir(path):
+            #check sub-#, check session folder, check extension
+            file_re = re.match("^(sub-([0-9]*))_([a-zA-Z0-9_-]*)_((s([0-9]*)_r([0-9]*))_e([0-9]*))((?:\.[a-zA-Z]+)*)$", raw_file)
+            if file_re:
+                if file_re.group(1) != sub:
+                    print(c.RED + "Error: file from subject", file_re.group(1), "found in", sub, "folder:", join(path, raw_file) + c.ENDC)
+                if file_re.group(5) != ses and len(ses) > 0:
+                    print(c.RED + "Error: file from session", file_re.group(5), "found in", ses, "folder:", join(path, raw_file) + c.ENDC)
+                if file_re.group(9) not in possible_exts and len(file_re.group(9)) > 0:
+                    print(c.RED + "Error: file with extension", file_re.group(9), "found, doesn\'t match expected extensions", ", ".join(possible_exts), ":", join(path, raw_file) + c.ENDC)
+                if file_re.group(2) != '' and not allowed_val(allowed_subs, file_re.group(2)):
+                    print(c.RED + "Error: subject number", file_re.group(2), "not an allowed subject value", allowed_subs, "in file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(3) not in dd_dict.keys():
+                    print(c.RED + "Error: variable name", file_re.group(3), "does not match any datadict variables, in file:", join(path, raw_file) + c.ENDC)
+                if datatype not in file_re.group(3):
+                    print(c.RED + "Error: variable name", file_re.group(3), "does not contain the name of the enclosing datatype folder", datatype, "in file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(4) not in allowed_suffixes:
+                    print(c.RED + "Error: suffix", file_re.group(4), "not in allowed suffixes", ", ".join(allowed_suffixes), "in file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(2) == "":
+                    print(c.RED + "Error: subject # missing from file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(3) == "":
+                    print(c.RED + "Error: variable name missing from file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(6) == "":
+                    print(c.RED + "Error: session # missing from file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(7) == "":
+                    print(c.RED + "Error: run # missing from file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(8) == "":
+                    print(c.RED + "Error: event # missing from file:", join(path, raw_file) + c.ENDC)
+                if file_re.group(9) == "":
+                    print(c.RED + "Error: extension missing from file, does\'nt match expected extensions", ", ".join(possible_exts), ":", join(path, raw_file) + c.ENDC)
+                if datatype == "psychopy" and file_re.group(9) == ".csv" and file_re.group(2) != "":
+                    # Call check-id.py for psychopy files
+                    subprocess.run(["python3", "check-id.py", file_re.group(2), join(path, raw_file)], shell=False)
+            else:
+                print(c.RED + "Error: file ", join(path, raw_file), " does not match naming convention <sub-#>_<variable/task-name>_<session>.<ext>" + c.ENDC)
+
 if __name__ == "__main__":
     dataset = sys.argv[1]
 
@@ -60,9 +119,8 @@ if __name__ == "__main__":
     allowed_subs = df_dd.loc["id", "allowedValues"]
 
     # now search sourcedata/raw for correct files
-    for key, values in dd_dict.items():
-        print("Verifying files for:", key)
-        variable = key
+    for variable, values in dd_dict.items():
+        print("Verifying files in raw for:", variable)
         datatype = values[0]
         allowed_suffixes = values[1].split(", ")
         fileexts = values[2].split(", ") # with or without . ?
@@ -84,63 +142,10 @@ if __name__ == "__main__":
                     if not re.match("^sub-[0-9]+$", subject):
                         print(c.RED + "Error: subject directory ", subject, " does not match sub-# convention" + c.ENDC)
                         continue
-                    obs_files = []
-                    corrected = False
-                    for raw_file in listdir(join(raw, ses, datatype, subject)):
-                        if re.match('^[Dd]eviation.*$', raw_file):
-                            corrected = True
-                        else:
-                            obs_files.append(raw_file)
-                    if corrected:
-                        print("deviation.txt seen in ", join(raw, ses, datatype, subject), ", skipping copy.")
-                        for raw_file in listdir(join(raw, ses, datatype, subject)):
-                            # still check that files are in the correct subject folder and session folder
-                            file_re = re.match("^(sub-[0-9]*)_([a-zA-Z0-9_-]*_)?(s[0-9]*_r[0-9]*)_e[0-9]*.*$", raw_file)
-                            if file_re and file_re.group(1) != subject:
-                                print(c.RED + "Error: file from subject", file_re.group(1), "found in", subject, "folder:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re and len(ses) > 0 and file_re.group(3) != ses:
-                                print(c.RED + "Error: file from session", file_re.group(3), "found in", ses, "folder:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                        continue
-                    if len(obs_files) > numfiles:
-                        print(c.RED + "Error: number of", datatype, "data files in subject folder", subject, str(len(obs_files)), "greater than the expected number", str(numfiles) + c.ENDC)
-                    elif len(obs_files) < numfiles:
-                        print(c.RED + "Error: number of", datatype, "data files in subject folder", subject, str(len(obs_files)), "less than the expected number", str(numfiles) + c.ENDC)
-                    for raw_file in listdir(join(raw, ses, datatype, subject)):
-                        #check sub-#, check session folder, check extension
-                        file_re = re.match("^(sub-([0-9]*))_([a-zA-Z0-9_-]*)_((s([0-9]*)_r([0-9]*))_e([0-9]*))((?:\.[a-zA-Z]+)*)$", raw_file)
-                        if file_re:
-                            if file_re.group(1) != subject:
-                                print(c.RED + "Error: file from subject", file_re.group(1), "found in", subject, "folder:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(5) != ses and len(ses) > 0:
-                                print(c.RED + "Error: file from session", file_re.group(5), "found in", ses, "folder:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(9) not in possible_exts and len(file_re.group(9)) > 0:
-                                print(c.RED + "Error: file with extension", file_re.group(9), "found, doesn\'t match expected extensions", ", ".join(possible_exts), ":", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(2) != '' and not allowed_val(allowed_subs, file_re.group(2)):
-                                print(c.RED + "Error: subject number", file_re.group(2), "not an allowed subject value", allowed_subs, "in file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(3) not in dd_dict.keys():
-                                print(c.RED + "Error: variable name", file_re.group(3), "does not match any datadict variables, in file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if datatype not in file_re.group(3):
-                                print(c.RED + "Error: variable name", file_re.group(3), "does not contain the name of the enclosing datatype folder", datatype, "in file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(4) not in allowed_suffixes:
-                                print(c.RED + "Error: suffix", file_re.group(4), "not in allowed suffixes", ", ".join(allowed_suffixes), "in file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(2) == "":
-                                print(c.RED + "Error: subject # missing from file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(3) == "":
-                                print(c.RED + "Error: variable name missing from file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(6) == "":
-                                print(c.RED + "Error: session # missing from file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(7) == "":
-                                print(c.RED + "Error: run # missing from file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(8) == "":
-                                print(c.RED + "Error: event # missing from file:", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if file_re.group(9) == "":
-                                print(c.RED + "Error: extension missing from file, does\'nt match expected extensions", ", ".join(possible_exts), ":", join(raw, ses, datatype, subject, raw_file) + c.ENDC)
-                            if datatype == "psychopy" and file_re.group(9) == ".csv" and file_re.group(2) != "":
-                                # Call check-id.py for psychopy files
-                                subprocess.run(["python3", "check-id.py", file_re.group(2), join(raw, ses, datatype, subject, raw_file)], shell=False)
-                        else:
-                            print(c.RED + "Error: file ", join(raw, ses, datatype, subject, raw_file), " does not match naming convention <sub-#>_<variable/task-name>_<session>.<ext>" + c.ENDC)
-
+                    path = join(raw, ses, datatype, subject)
+                    # check that files in raw match conventions
+                    check_filenames(path, subject, ses, datatype, allowed_suffixes, possible_exts, numfiles)
+                    # copy to checked
                     for suffix in allowed_suffixes:
                         presence = False
                         copied_files = []
@@ -160,4 +165,29 @@ if __name__ == "__main__":
             else:
                 print(c.RED + "Error: can\'t find ", datatype, "directory under ", raw, "/", ses + c.ENDC)
 
-        
+    # do same filename checks for checked files
+    for variable, values in dd_dict.items():
+        print("Verifying files in checked for:", variable)
+        variable = variable
+        datatype = values[0]
+        allowed_suffixes = values[1].split(", ")
+        fileexts = values[2].split(", ") # with or without . ?
+        allowed_vals = values[3].split(", ")
+        possible_exts = sum([ext.split('|') for ext in fileexts], [])
+        numfiles = len(fileexts)
+
+        if sessions:
+            expected_sessions = []
+            for ses in allowed_suffixes:
+                ses_re = re.match("(s[0-9]+_r[0-9]+)(_e[0-9]+)?", ses)
+                if ses_re:
+                    expected_sessions.append(ses_re.group(1))
+        else:
+            expected_sessions = [""]
+        for sub in listdir(checked):
+            if sub.startswith("sub-"):
+                for ses in expected_sessions:
+                    if isdir(join(checked, sub, ses, datatype)):
+                        # check that files in checked match conventions
+                        path = join(checked, sub, ses, datatype)
+                        check_filenames(path, sub, ses, datatype, allowed_suffixes, possible_exts, numfiles)
