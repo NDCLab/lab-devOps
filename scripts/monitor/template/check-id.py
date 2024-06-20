5fd9179c74f14633bf7f1a914840592e684801fb
David Hunt <davhunt@iu.edu>
2024-06-20 15:38:08 -0400

    coor code errors

--- scripts/monitor/template/check-id.py
+++ scripts/monitor/template/check-id.py
@@ -3,6 +3,13 @@ import pandas as pd
 import math
 import sys
 
+class c:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 if __name__ == "__main__":
     id = sys.argv[1]
     file = sys.argv[2]
@@ -17,11 +24,11 @@ if __name__ == "__main__":
     elif "participant" in file_df:
         id_col = file_df["participant"]
     else:
-        sys.exit("Error: cannot find id or participant column in"+ file)
+        sys.exit(c.RED + "Error: cannot find id or participant column in"+ file + c.ENDC)
 
     # check if first ids match vals listed
     if isinstance(id_col[0], float) and math.isnan(id_col[0]):
-        print("Error: nan value seen in ID for", file, "file")
+        print(c.RED + "Error: nan value seen in ID for", file, "file" + c.ENDC)
     else:
         if not int(id_col[0]) == int(id):
-            print("Error: ID value in", file, str(id_col[0]), "does not match", id)
+            print(c.RED + "Error: ID value in", file, str(id_col[0]), "does not match", id + c.ENDC)
