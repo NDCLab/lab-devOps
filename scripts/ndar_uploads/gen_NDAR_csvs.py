799346762525980a34136c932700254a159500f7
David Hunt <davhunt@iu.edu>
2024-07-15 11:20:57 -0400

    add EEG zipping scripts, thrive and read ndar uploads

--- scripts/ndar_uploads/gen_NDAR_csvs.py
+++ scripts/ndar_uploads/gen_NDAR_csvs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 import os
-from os.path import basename, join
+from os.path import basename, join, isdir
 
 import sys
 import json
@@ -11,21 +11,30 @@ import numpy as np
 from datetime import datetime
 
 
-def get_redcaps(datadict_df, redcaps):
+def get_redcaps(datadict_df, redcaps, ndar_json, other_sessions=False):
     df = datadict_df
     redcaps_dict = {}
-    for _, row in df.iterrows():
-        if row["dataType"] not in ["consent", "assent", "redcap_data"]: #just redcap data
-            continue
-        prov = row["provenance"].split(" ")
-        if "file:" in prov and "variable:" in prov:
-            idx = prov.index("file:")
-            rc_filename = prov[idx+1].strip("\";")
-            if not rc_filename in redcaps_dict.keys():
-                redcaps_dict[rc_filename] = {}
-        else:
-            continue
-    for expected_rc in redcaps_dict.keys():
+    if not other_sessions:
+        for _, row in df.iterrows():
+            if row["dataType"] not in ["consent", "assent", "redcap_data"]: #just redcap data
+                continue
+            prov = row["provenance"].split(" ")
+            if "file:" in prov and "variable:" in prov:
+                idx = prov.index("file:")
+                rc_filename = prov[idx+1].strip("\";,")
+                if not rc_filename in redcaps_dict.keys():
+                    redcaps_dict[rc_filename] = {}
+            else:
+                continue
+        rcs_to_look_for = redcaps_dict.keys()
+    else:
+        redcaps_from_other_sessions = []
+        for out_csv in ndar_json.keys():
+            for col in ndar_json[out_csv]["req_columns"]:
+                if "different_session" in ndar_json[out_csv]["req_columns"][col].keys() and ndar_json[out_csv]["req_columns"][col]["redcap"] not in redcaps_from_other_sessions:
+                    redcaps_from_other_sessions.append(ndar_json[out_csv]["req_columns"][col]["redcap"])
+        rcs_to_look_for = redcaps_from_other_sessions
+    for expected_rc in rcs_to_look_for:
         present = False
         for redcap in redcaps:
             if expected_rc in basename(redcap.lower()) and present == False:
@@ -33,23 +42,31 @@ def get_redcaps(datadict_df, redcaps):
                 redcaps_dict[expected_rc] = pd.read_csv(redcap_path, index_col="record_id")
                 present = True
             elif expected_rc in basename(redcap.lower()) and present == True:
-                sys.exit("Error: multiple redcaps found with name specified in datadict, " + redcap_path + " and " + redcap + ", exiting.")
+                sys.error("Error: multiple redcaps found with name specified in datadict, " + redcap_path + " and " + redcap + ", exiting.")
         if present == False:
             sys.exit("Error: can't find redcap specified in datadict " + expected_rc + ", exiting.")
     return redcaps_dict
 
-def map_race(ndar_df, ndar_json, sre):
+
+def map_race(ndar_df, ndar_json, redcap, race_col, sre, sessionless=False, parent=False):
     race_dict = { "10": "White", "11": "Black or African American", "12": "American Indian/Alaska Native", "13": "American Indian/Alaska Native", \
                   "14": "Hawaiian or Pacific Islander", "15": "Hawaiian or Pacific Islander", "16": "Hawaiian or Pacific Islander", \
                   "17": "Hawaiian or Pacific Islander", "18": "Asian", "19": "Asian", "20": "Asian", "21": "Asian", "22": "Asian", "23": "Asian", \
                   "24": "Asian", "25": "Other Non-White", "999": "Unknown or not reported" }
-    rc_df = redcaps_dict['iqsparent']
+    rc_df = redcaps_dict[redcap]
+    if sessionless:
+        race_col_base = Column(race_col)
+    else:
+        race_col_base = Column(race_col + "_" + sre)
     race_cols = []
     for col in rc_df.columns:
-        if re.match('demo(es)?_d_race_s[0-9]+_r[0-9]+_e[0-9]+.*', col):
+        if col.startswith(race_col_base.col) or col.startswith(race_col_base.coles):
             race_cols.append(col)
-    for id in rc_df.index:
-        child_id = int(str(id)[0:2] + "0" + str(id)[3:])
+    for child_id in ndar_df.index:
+        if parent:
+            id = int(str(child_id)[0:2] + "8" + str(child_id)[3:])
+        else:
+            id = child_id
         sum = 0
         for col in race_cols:
             sum += rc_df.loc[id, col]
@@ -57,16 +74,22 @@ def map_race(ndar_df, ndar_json, sre):
             for col in race_cols:
                 if rc_df.loc[id, col] == 1:
                     race_num = re.match('^demo(es)?_d_race_s[0-9]+_r[0-9]+_e[0-9]+_+([0-9]+)$', col).group(2)
+                    #race_num = re.match('^demo(es)?_e_race_s[0-9]+_r[0-9]+_e[0-9]+_+([0-9]+)$', col).group(2)
+                    # TODO this needs to be drawn from the JSON
+                    #########################################
                     race = race_dict[race_num]
                     ndar_df.loc[child_id, "race"] = race
         elif sum > 1:
             ndar_df.loc[child_id, "race"] = "More than one race"
+        elif sum == 0:
+            ndar_df.loc[child_id, "race"] = "Unknown or not reported"
 
 
-def map_interview_date(ndar_df, ndar_json, sre):
-    rc_df = redcaps_dict['iqsparent']
-    rc_variable = "infosht_" + sre + "_timestamp"
-    rc_variable_es = "infoshtes_" + sre + "_timestamp"
+def map_interview_date(ndar_df, ndar_json, sre, rc, rc_col):
+    rc_df = redcaps_dict[rc]
+    rc_col = Column(rc_col)
+    rc_variable = rc_col.col
+    rc_variable_es = rc_col.coles
     for id in rc_df.index:
         child_id = int(str(id)[0:2] + "0" + str(id)[3:])
         date_string = rc_df.loc[id, rc_variable]
@@ -109,11 +132,6 @@ def map_vals(ndar_df, ndar_col, ndar_csv, ndar_json, sre, parent=False):
             child_id = int(str(id)[0:2] + "0" + str(id)[3:])
         else:
             child_id = id
-        # fill in session (i.e. "s1") in timepoint_label in ndar_subject01
-        if ndar_col == "timepoint_label" and ndar_csv == "ndar_subject01":
-            sess = sre[0:2]
-            ndar_df.loc[child_id, ndar_col] = sess
-            continue
         if isinstance(rc_df, pd.core.frame.DataFrame):
             if id not in rc_df.index:
                 ndar_df.loc[child_id, ndar_col] = "" # "NA" ?
@@ -156,23 +174,24 @@ def map_vals(ndar_df, ndar_col, ndar_csv, ndar_json, sre, parent=False):
         if "conditional_column" in ndar_json[ndar_csv]["req_columns"][ndar_col].keys() and "conditional_column_mapping" in ndar_json[ndar_csv]["req_columns"][ndar_col].keys():
             # should really do this and "mapping" in a separate function
             conditional_rc = ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"]["redcap"]
-            conditional_rc_variable = ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"]["rc_variable"]
+            if "sessionless" in ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"].keys():
+                conditional_rc_variable = ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"]["rc_variable"]
+            else:
+                conditional_rc_variable = ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"]["rc_variable"] + "_" + sre
             try:
                 conditional_rc_df = redcaps_dict[conditional_rc]
             except KeyError:
                 sys.exit("Can't find redcap " + conditional_rc + ", exiting.")
             for col in conditional_rc_df.columns:
-                if col.startswith(conditional_rc_variable+"_"):
+                if col == conditional_rc_variable:
                     conditional_rc_column = col
                     if "parent" in ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"] and ndar_json[ndar_csv]["req_columns"][ndar_col]["conditional_column"]["parent"].lower() == 'true':
                         conditional_rc_column_es = Column(conditional_rc_column).coles
                     else:
                         conditional_rc_column_es = math.nan
-                    # TODO: deal with when more than one column starts with cond_rc_variable (e1 and e2, etc?)
                     break
             conditional_rc_id = int(str(id)[0:2] + str(conditional_rc_df.index[0])[2] + str(id)[3:]) # conditional redcap could be parent or child, 300's or 308's or 309's
             val = conditional_rc_df.loc[conditional_rc_id, conditional_rc_column]
-            #if math.isnan(val) and not math.isnan(conditional_rc_df.loc[conditional_rc_id, conditional_rc_column_es]): # look at "es" surveys too if it's a parent survey
             if math.isnan(val) and conditional_rc_column_es in conditional_rc_df.columns and not math.isnan(conditional_rc_df.loc[conditional_rc_id, conditional_rc_column_es]): # look at "es" surveys too if it's a parent survey
                 val = conditional_rc_df.loc[conditional_rc_id, conditional_rc_column_es]
             if (isinstance(val, float) or isinstance(val, int)) and not math.isnan(val):
@@ -195,7 +214,7 @@ def map_vals(ndar_df, ndar_col, ndar_csv, ndar_json, sre, parent=False):
             if "mapping_formula" in ndar_json[ndar_csv]["req_columns"][ndar_col].keys():
                 x = rc_df.loc[id, rc_column]
                 val = eval(ndar_json[ndar_csv]["req_columns"][ndar_col]["mapping_formula"])
-                if -0.001 < val-round(val) < 0.001: # don't round if val is a decimal
+                if -0.01 < val-round(val) < 0.01: # don't round if val is a decimal
                     val = str(int(val))
                 ndar_df.loc[child_id, ndar_col] = val
                 continue
@@ -221,6 +240,7 @@ def map_adis(ndar_df, ndar_col, ndar_csv, ndar_json, sre, all_columns=False, par
     for col in ndar_json[ndar_csv]["req_columns"].keys():
         ndar_df.loc[:, col] = 0
     for id in ndar_df.index:
+    #for id in list(rc_df.index):
         diagnoses = []
         for i in range(1, 9):
             val = rc_df.loc[id, "adis_fn_dx" + str(i) + "_lb_" + sre]
@@ -247,7 +267,8 @@ def save_csv(ndar_csv, ndar_df):
     f = open('tmpfile.csv', 'r')
     csvstring = f.read()
     f.close()
-    with open(join(out_path, ndar_csv + '_incomplete.csv'), 'w') as f:
+    #with open(join(out_path, ndar_csv + '_incomplete.csv'), 'w') as f:
+    with open(join(out_path, ndar_csv + '_' + sre + '_incomplete.csv'), 'w') as f:
         f.write(ndar_csv[0:-2] + ",01" + ","*(len(df.columns)-2) + "\n")
         f.write(csvstring)
     f.close()
@@ -262,6 +283,8 @@ class Column:
         else:
             after_es_str = col_re.group(2) if col_re.group(2) else ""
             self.coles = col_re.group(1) + "es" + after_es_str
+    def __str__(self):
+        return self.col
 
 
 if __name__ == "__main__":
@@ -270,39 +293,56 @@ if __name__ == "__main__":
     ndar_json = sys.argv[3] # json with mapping info
     sre = sys.argv[4] # session run event, like "s1_r1_e1"
     out_path = sys.argv[5] # output folder for CSVs
+    if len(sys.argv) == 7:
+        redcaps_other_sessions = sys.argv[6] # full filenames of any redcaps needed that aren't from the session from "sre" (comma-seperated)
 
+    if not isdir(out_path):
+        os.mkdir(out_path)
     redcaps = redcaps.split(',')
     df_dd = pd.read_csv(df_dd)
     with open(ndar_json, 'r') as json_file:
         ndar_json = json.load(json_file)
     json_file.close()
 
-    redcaps_dict = get_redcaps(df_dd, redcaps) # dataframes of each redcap
+    redcaps_dict = get_redcaps(df_dd, redcaps, ndar_json) # dataframes of each redcap
+    if 'redcaps_other_sessions' in locals() and redcaps_other_sessions.lower() != "none":
+        redcaps_other_sessions = redcaps_other_sessions.split(',')
+        redcaps_dict_other_sessions = get_redcaps(df_dd, redcaps_other_sessions, ndar_json, other_sessions=True)
+        if len(set(redcaps_dict.keys()).intersection(set(redcaps_dict_other_sessions.keys()))) != 0:
+            sys.exit("error......")
+        else:
+            redcaps_dict.update(redcaps_dict_other_sessions)
+
 
     if "src_subject_id" in ndar_json["all"]["req_columns"].keys(): # src_subject_id required to get ids/indices at least for thrive
         redcap = ndar_json["all"]["req_columns"]["src_subject_id"]["redcap"]
-        for rc in redcaps:
-            if redcap in basename(rc).lower():
-                id_redcap = pd.read_csv(rc, index_col = ndar_json["all"]["req_columns"]["src_subject_id"]["rc_variable"]) # record_id
-                ids = list(id_redcap.index)
-                break
+        if redcap in redcaps_dict.keys():
+            id_redcap = redcaps_dict[redcap]
+            ids = list(id_redcap.index)
         # for thrive, drop rows who haven't filled out infosht
-        if "infosht_" + sre + "_complete" in id_redcap.columns:
-            complete_infosht_ids = []
-            for id in ids:
-                if id_redcap.loc[id,"infosht_" + sre + "_complete"] == 2 or id_redcap.loc[id,"infoshtes_" + sre + "_complete"] == 2:
-                    complete_infosht_ids.append(id)
-            ids = [int(str(id)[0:2] + "0" + str(id)[3:]) for id in complete_infosht_ids] #quick fix to parent ids -> child ids
+        rc_variable = ndar_json["all"]["req_columns"]["src_subject_id"]["rc_variable"]
+        rc_variable_es = rc_variable.split("_")[0] + "es_" + "_".join(rc_variable.split("_")[1:])
+        if not "sessionless" in ndar_json["all"]["req_columns"]["src_subject_id"].keys():
+            rc_variable = rc_variable + "_" + sre + "_complete"
+            rc_variable_es = rc_variable + "es_" + sre + "_complete"
+        complete_infosht_ids = []
+        for id in ids:
+            if id_redcap.loc[id, rc_variable] == 2 or id_redcap.loc[id, rc_variable_es] == 2:
+                complete_infosht_ids.append(id)
+        ids = [int(str(id)[0:2] + "0" + str(id)[3:]) for id in complete_infosht_ids] #quick fix to parent ids -> child ids
+
     for ndar_csv in ndar_json.keys():
+        if ndar_csv == "all":
+            continue
         ndar_columns = ndar_json[ndar_csv]["all_columns"]
         df = pd.DataFrame(columns = ndar_columns, index = ids)
         if ndar_csv == "adis_v01":
             map_adis(df, col, ndar_csv, ndar_json, sre)
-            save_csv(ndar_csv, df)
-            continue
         for col in ndar_json["all"]["req_columns"].keys():
             if col == "interview_date":
-                map_interview_date(df, ndar_json, sre)
+                rc = ndar_json["all"]["req_columns"]["interview_date"]["redcap"]
+                rc_col = ndar_json["all"]["req_columns"]["interview_date"]["rc_variable"]
+                map_interview_date(df, ndar_json, sre, rc, rc_col)
                 continue
             if col == "interview_age":
                 df.loc[:, col] = "" # just ignore for now
@@ -315,9 +355,21 @@ if __name__ == "__main__":
                 parent_col = True
             map_vals(df, col, "all", ndar_json, sre, parent=parent_col)
         for col in ndar_json[ndar_csv]["req_columns"]:
-            if col == "race":
-                map_race(df, ndar_json, sre)
+            if col in ndar_json["all"]["req_columns"].keys(): # already mapped columns in all ^^
                 continue
+            if col == "race":
+                race_redcap = ndar_json[ndar_csv]["req_columns"][col]["redcap"]
+                race_column = ndar_json[ndar_csv]["req_columns"][col]["rc_variable"]
+                sessionless = True if "sessionless" in ndar_json[ndar_csv]["req_columns"][col].keys() else False
+                parent = True if "parent" in ndar_json[ndar_csv]["req_columns"][col].keys() else False
+                map_race(df, ndar_json, race_redcap, race_column, sre, sessionless=sessionless, parent=parent)
+                continue
+            if col == "timepoint_label":
+                sess = sre[0:2]
+                df.loc[:, col] = sess
+                continue
+            if "mapping" in ndar_json[ndar_csv]["req_columns"][col].keys() and ndar_json[ndar_csv]["req_columns"][col]["mapping"] == "custom":
+                continue # "custom" mappings should be done by here
             parent_col = False
             if "parent" in ndar_json[ndar_csv]["req_columns"][col] and ndar_json[ndar_csv]["req_columns"][col]["parent"].lower() == "true":
                 parent_col = True
