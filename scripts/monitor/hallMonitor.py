5bd41d80e9680b111230b5b56915ffb3ec905d27
Nolan Welch <nolanwelch@outlook.com>
2024-10-23 19:16:57 -0400

    fix dd_df legal variable check

--- scripts/monitor/hallMonitor.py
+++ scripts/monitor/hallMonitor.py
@@ -173,7 +173,7 @@ def validate_data(logger, dataset, is_raw=True):
         logger.debug("Initialized id_dir as %s", id_dir)
         if id_dir not in logged_missing_ids:
             logged_missing_ids[id_dir] = set()
-        if id.variable not in dd_df["variable"]:
+        if dd_df[dd_df["variable"] == id.variable].empty:
             pending.append(
                 new_error_record(
                     logger,
