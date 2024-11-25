90b34cbd881aee2a16c874c08311952bcf799b86
Nolan Welch <nolanwelch@outlook.com>
2024-11-24 19:14:51 -0500

    move main testing script to pytest

--- scripts/monitor/gen_bug_tracking.py
+++ scripts/monitor/gen_bug_tracking.py
@@ -1,8 +1,8 @@
-import argparse
 import os
 from typing import Type
 
 import pandas as pd
+import pytest
 from test_cases import (
     DeviationAndNoDataErrorTestCase,
     DeviationAndNoDataFilesErrorTestCase,
@@ -42,12 +42,6 @@ from test_cases import (
 )
 
 BASE_SUBJECT_ID = TestCase.BASE_SUBJECT_ID
-
-
-def create_tests(tests: list[Type[TestCase]], basedir: str):
-    registry = TestCaseRegistry(basedir)
-    registry.add_cases(tests)
-    registry.generate_all()
 
 
 def create_base_subject(basedir):
@@ -138,37 +132,22 @@ def create_base_subject(basedir):
             f.write("digi data")
 
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description="This script generates base subject data, as well as test data, given a list of test cases."
-    )
-    parser.add_argument(
-        "basedir",
-        type=validated_dir,
-        help="the destination directory for generated data",
-    )
+def create_tests():
+    basedir = "test_output"
+    if os.path.exists(basedir):
+        # clear directory before running tests
+        for root, dirs, files in os.walk(basedir, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+    os.makedirs(basedir, exist_ok=True)
 
-    return parser.parse_args()
-
-
-def validated_dir(input):
-    basedir = os.path.realpath(input)
-    if not os.path.exists(basedir):
-        raise argparse.ArgumentTypeError(f"{basedir} does not exist")
-    elif not os.path.isdir(basedir):
-        raise argparse.ArgumentTypeError(f"{basedir} is not a directory")
-    return basedir
-
-
-if __name__ == "__main__":
-    args = get_args()
-    basedir = str(args.basedir)
-
-    if os.listdir(basedir):
-        raise FileExistsError(f"{basedir} is not empty, clear files before running")
-
+    # one-time setup for the base subject
     create_base_subject(basedir)
 
+    # set up test cases
+    registry = TestCaseRegistry(basedir)
     tests: list[Type[TestCase]] = [
         DeviationAndNoDataErrorTestCase,
         DeviationAndNoDataFilesErrorTestCase,
@@ -204,5 +183,16 @@ if __name__ == "__main__":
         NoDataAdditionalFilesTestCase,
         PsychopyFileIDMismatchTestCase,
     ]
+    registry.add_cases(tests)
+    registry.generate_all()
+    return registry
 
-    create_tests(tests, basedir)
+
+registry = create_tests()
+tests = registry.get_cases()
+
+
+# run each test separately using a pytest harness
+@pytest.mark.parametrize("test_case", tests)
+def test_validate_test_cases(test_case):
+    test_case.validate()
