f5e37523e138a2ab8f2998676a14a6da18112c8f
Nolan Welch <nolanwelch@outlook.com>
2024-11-27 17:06:48 -0500

    define errors for MissingVariableNameTestCase

--- scripts/monitor/hallMonitor2/tests/integration/hm_test_cases.py
+++ scripts/monitor/hallMonitor2/tests/integration/hm_test_cases.py
@@ -434,6 +434,27 @@ class MissingVariableNameTestCase(FileNameTestCase):
 
         return modified_files
 
+    def get_expected_errors(self):
+        basename = f"sub-{self.sub_id}_PATTERN_s1_r1_e1.csv"
+        old_var = "arrow-alert-v1-1_psychopy"
+        new_var = ""
+        old_basename = basename.replace("PATTERN", old_var)
+        new_basename = basename.replace("PATTERN", new_var)
+
+        naming_info = re.escape(
+            f"File {new_basename} does not match expected identifier format"
+        )
+        missing_info = re.escape(f"Expected file {old_basename} not found")
+        extra_info = re.escape(f"Unexpected file {new_basename} found")
+
+        errors = [
+            ExpectedError("Naming error", naming_info),
+            ExpectedError("Missing file", missing_info),
+            ExpectedError("Unexpected file", extra_info),
+        ]
+
+        return errors
+
 
 class MissingSubjectNumberTestCase(FileNameTestCase):
     """
