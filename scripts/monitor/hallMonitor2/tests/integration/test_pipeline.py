6c8337d8b521eec9716388f862d4d28f974f9a33
Nolan Welch <nolanwelch@outlook.com>
2024-12-27 22:28:31 -0500

    add BBSDataOneDeviationFileTestCase

--- scripts/monitor/hallMonitor2/tests/integration/test_pipeline.py
+++ scripts/monitor/hallMonitor2/tests/integration/test_pipeline.py
@@ -70,6 +70,7 @@ from .qa_cases import (
 )
 from .tracker_cases import (
     BaseUpdateTrackerTestCase,
+    BBSDataOneDeviationFileTestCase,
     BBSDataZeroIncorrectDataTestCase,
     BBSDataZeroMissingDataTestCase,
     BBSDataZeroMissingDatatypeFolderTestCase,
@@ -537,6 +538,7 @@ def create_registry():
     tests: list[Type[TestCase]] = [
         BaseTestCase,
         BaseUpdateTrackerTestCase,
+        BBSDataOneDeviationFileTestCase,
         BBSDataZeroIncorrectDataTestCase,
         BBSDataZeroMissingDataTestCase,
         BBSDataZeroMissingDatatypeFolderTestCase,
