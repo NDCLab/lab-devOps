ac965c81923d0f1ba07abb03355dbb7ed626661e
Nolan Welch <nolanwelch@outlook.com>
2025-01-16 19:12:20 -0500

    remove log output from backup script

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -3,7 +3,6 @@
 SINGULARITY_VERSION="3.8.2"
 PYTHON_CONTAINER="/home/data/NDClab/tools/containers/python-3.9/python-3.9.simg"
 BACKUP_PYSCRIPT="/home/data/NDClab/tools/lab-devOps/scripts/backup/backup.py"
-OUT_FILE=/home/data/NDClab/other/logs/backups/$(date +%m_%d_%Y::%H:%M:%S).log
 
 module load "singularity-$SINGULARITY_VERSION"
-singularity exec -e "$PYTHON_CONTAINER" python3 -u "$BACKUP_PYSCRIPT" >"$OUT_FILE"
+singularity exec -e "$PYTHON_CONTAINER" python3 -u "$BACKUP_PYSCRIPT"
