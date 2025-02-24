cbd59e22e9d427723673a7d4931cef70d90d8856
Nolan Welch <nolanwelch@outlook.com>
2025-02-24 15:18:32 -0500

    Fix hallmonitor version in setup.py

--- scripts/monitor/hallMonitor2/hallmonitor/setup.py
+++ scripts/monitor/hallMonitor2/hallmonitor/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="hallmonitor",
-    version="2.0.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "pytest",
