65458b9e4b1f0bf92e792dfbea96227d496a96db
Nolan Welch <nolanwelch@outlook.com>
2025-03-06 20:59:07 -0500

    Add labels submodule

---
+++ scripts/python/syllable-match/syllable_match/labels/errors.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def label_errors(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Labels errors in the DataFrame.
+    """
+    return df
---
+++ scripts/python/syllable-match/syllable_match/labels/hesitations.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def label_hesitations(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Labels hesitations in the DataFrame.
+    """
+    return df
