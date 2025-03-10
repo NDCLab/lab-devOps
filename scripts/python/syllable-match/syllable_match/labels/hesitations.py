ba1d8abc4a38ce2fdb13b6b015ec91764f6727e7
Nolan Welch <nolanwelch@outlook.com>
2025-03-10 00:24:17 -0400

    Implement hesitation labeling

--- scripts/python/syllable-match/syllable_match/labels/hesitations.py
+++ scripts/python/syllable-match/syllable_match/labels/hesitations.py
@@ -3,6 +3,29 @@ import pandas as pd
 
 def label_hesitations(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Labels hesitations in the DataFrame.
+    Identifies and labels hesitations within a given DataFrame. This function marks the start and end of hesitations,
+    and assigns a unique index to each hesitation sequence. The output DataFrame includes three new columns:
+    'hesitation-start', 'hesitation-end', and 'hesitation-idx'.
     """
+
+    # Initialize 'hesitation-start' column by copying 'hesitation-disfluency' values
+    df["hesitation-start"] = df["hesitation-disfluency"]
+
+    # Determine 'hesitation-end' by shifting 'hesitation-start' down
+    #    by one row and filling NaNs with 0
+    df["hesitation-end"] = df["hesitation-start"].shift(-1).fillna(0)
+
+    # Assign a unique index to each hesitation sequence
+    df["hesitation-idx"] = (
+        df[df["hesitation-start"].astype(bool) | df["hesitation-end"].astype(bool)]
+        .groupby("hesitation-start")
+        .cumcount()
+        + 1
+    )
+    # For rows without hesitation, set 'hesitation-idx' to NaN
+    df.loc[
+        ~(df["hesitation-start"].astype(bool) | df["hesitation-end"].astype(bool)),
+        "hesitation-idx",
+    ] = None
+
     return df
