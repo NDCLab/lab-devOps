e8a27951d8957669216941f51b65d3828e124c41
Nolan Welch <nolanwelch@outlook.com>
2025-03-10 17:04:36 -0400

    Implement label_errors

--- scripts/python/syllable-match/syllable_match/labels/errors.py
+++ scripts/python/syllable-match/syllable_match/labels/errors.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from syllable_match.utils import compute_window_indicator
+
 
 def label_errors(df: pd.DataFrame) -> None:
     """
@@ -47,3 +49,66 @@ def label_errors(df: pd.DataFrame) -> None:
             (df["correction-syll"] == 1) & (df["correction-syll"].shift(-1) == 0)
         )
     )
+
+    # Make additional markings for high and low errors
+    high_error_idx = 0
+    low_error_idx = 0
+    for idx, row in df.iterrows():
+        if row["low-error"] == 1:
+            # If any attempt was made to correct the error...
+            if row["correction-syll"] == 1:
+                row["low-error-corrected"] = 1
+            # If prior syllable was a high error or a comparison or had an allowable disfluency...
+            if idx > 0 and (
+                (df[idx - 1]["high-error"] == 1)
+                or (df[idx - 1]["any-deviation"] == 0)
+                or df[idx - 1]["allowable-disfluency"] == 1
+            ):
+                row["low-error-start"] = 1
+                low_error_idx += 1
+                row["low-error-idx"] = low_error_idx
+            # If prior syllable was a low error...
+            if idx > 0 and df[idx - 1]["low-error"] == 1:
+                # Copy the prior syllable's low error index
+                row["low-error-idx"] = df[idx - 1]["low-error-idx"]
+            # If syllable is marked as an allowable disfluency, or if next syllable is a comparison,
+            #   or if the next syllable is a high error...
+            if (
+                row["allowable-disfluency"] == 1
+                or df[idx + 1]["any-deviation"] == 0
+                or df[idx + 1]["high-error"] == 1
+            ):
+                row["low-error-end"] = 1
+
+        if row["high-error"] == 1:
+            # If any attempt was made to correct the error...
+            if row["correction-syll"] == 1:
+                row["high-error-corrected"] = 1
+            # If prior syllable was a low error or a comparison or had an allowable disfluency...
+            if idx > 0 and (
+                (df[idx - 1]["low-error"] == 1)
+                or (df[idx - 1]["any-deviation"] == 0)
+                or df[idx - 1]["allowable-disfluency"] == 1
+            ):
+                row["high-error-start"] = 1
+                high_error_idx += 1
+                row["high-error-idx"] = high_error_idx
+            # If prior syllable was a high error...
+            if idx > 0 and df[idx - 1]["high-error"] == 1:
+                row["high-error-idx"] = df[idx - 1]["high-error-idx"]
+            # If syllable is marked as an allowable disfluency, or if next syllable is a comparison,
+            #   or if next syllable is a low error...
+            if (
+                row["allowable-disfluency"] == 1
+                or df[idx + 1]["any-deviation"] == 0
+                or df[idx + 1]["low-error"] == 1
+            ):
+                row["high-error-end"] = 1
+
+        # Generate before/after indicators with a window size of 7
+        df["high-error-before"], df["high-error-after"] = compute_window_indicator(
+            df["high-error"], 7
+        )
+        df["low-error-before"], df["low-error-after"] = compute_window_indicator(
+            df["low-error"], 7
+        )
