85ad5ae668b5c2109122765ecf7a24fbed251e39
Nolan Welch <nolanwelch@outlook.com>
2025-03-10 16:44:39 -0400

    Implement label_errors

--- scripts/python/syllable-match/syllable_match/labels/errors.py
+++ scripts/python/syllable-match/syllable_match/labels/errors.py
@@ -1,8 +1,49 @@
 import pandas as pd
 
 
-def label_errors(df: pd.DataFrame) -> pd.DataFrame:
+def label_errors(df: pd.DataFrame) -> None:
     """
     Labels errors in the DataFrame.
     """
-    return df
+    # Label low errors
+    df["low-error"] = (
+        ((df["Error_Misproduction"] > 0) & (df["Outcome_WordSubstitution"] == 0))
+        | ((df["Error_WordStressError"] > 0) & (df["Outcome_WordSubstitution"] == 0))
+        | (
+            (df["Error_InsertedSyllable"] > 0)
+            & (df["Error_InsertedWord"] == 0)
+            & (df["Outcome_WordSubstitution"] == 0)
+        )
+        | (
+            (df["Error_OmittedSyllable"] > 0)
+            & (df["Error_OmittedWord"] == 0)
+            & (df["Outcome_WordSubstitution"] == 0)
+        )
+    )
+
+    # Label high errors
+    df["high-error"] = (
+        (df["Error_Misproduction"] > 0) & (df["Outcome_WordSubstitution"] > 0)
+        | ((df["Error_WordStressError"] > 0) & (df["Outcome_WordSubstitution"] > 0))
+        | ((df["Error_InsertedSyllable"] > 0) & (df["Outcome_WordSubstitution"] > 0))
+        | ((df["Error_OmittedSyllable"] > 0) & (df["Outcome_WordSubstitution"] > 0))
+        | (df["Error_InsertedWord"] > 0)
+        | (df["Error_OmittedWord"] > 0)
+    )
+
+    # Label allowable disfluencies
+    df["allowable-disfluency"] = (
+        (df["Disfluency_InsertedProsodicBreak"] > 0)
+        | (df["Disfluency_FilledPause"] > 0)
+        | (df["Disfluency_Hesitation"] > 0)
+        | (df["Disfluency_Elongation"] > 0)
+        | (
+            # Current syllable is a duplication, but next syllable is not
+            (df["Disfluency_DuplicationElongationSyllable"] > 0)
+            & (df["Disfluency_DuplicationElongationSyllable"].shift(-1) == 0)
+        )
+        | (
+            # Current syllable is part of a correction, but next syllable is not
+            (df["correction-syll"] == 1) & (df["correction-syll"].shift(-1) == 0)
+        )
+    )
