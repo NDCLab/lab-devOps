df4cd9721a0746ffdcba62ee7aeb04d1474781cb
Nolan Welch <nolanwelch@outlook.com>
2025-03-10 17:07:44 -0400

    Save labels module

--- scripts/python/syllable-match/syllable_match/labels/__init__.py
+++ scripts/python/syllable-match/syllable_match/labels/__init__.py
@@ -1,2 +1,5 @@
+from .duplications import label_duplications
 from .errors import label_errors
 from .hesitations import label_hesitations
+
+__all__ = ["label_errors", "label_hesitations", "label_duplications"]
