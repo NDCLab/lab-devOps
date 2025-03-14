7cb8a1bb5ba8cd31e509865189a791735e3db68b
Nolan Welch <nolanwelch@outlook.com>
2025-03-13 22:19:51 -0400

    Init new files in matching module

--- scripts/python/syllable-match/syllable_match/matching/__init__.py
+++ scripts/python/syllable-match/syllable_match/matching/__init__.py
@@ -1,4 +1,6 @@
+from .duplications import match_duplications
+from .errors import match_errors
 from .hesitations import match_hesitations
 from .matcher import SyllableMatcher
 
-__all__ = ["match_hesitations", "SyllableMatcher"]
+__all__ = ["match_hesitations", "SyllableMatcher", "match_errors", "match_duplications"]
