715b1002f09a98d7f8015b7d55029a8ed0c705c5
Nolan Welch <nolanwelch@outlook.com>
2025-03-20 16:07:12 -0400

    Move build.sh to Makefile, .gitignore build/ dir

--- scripts/monitor/hallMonitor2/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get version from pyproject.toml
-VERSION=$(grep -oP '(?<=version = ")[^"]+' pyproject.toml | tr '.' '-')
-
-# Build the Singularity container
-sudo singularity build "hm2_${VERSION}.sif" Singularity.def
