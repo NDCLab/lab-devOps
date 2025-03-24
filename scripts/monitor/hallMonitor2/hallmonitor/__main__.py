ff92c6cd37160f602ca2dfa0da018c5325130ac4
Nolan Welch <nolanwelch@outlook.com>
2025-03-24 15:54:34 -0400

    Fix error handling in __main__.py

--- scripts/monitor/hallMonitor2/hallmonitor/__main__.py
+++ scripts/monitor/hallMonitor2/hallmonitor/__main__.py
@@ -1,7 +1,11 @@
+import sys
+import traceback
+
 from hallmonitor import app, hmutils
 
 try:
     app.main(hmutils.get_args())
 except Exception as e:
     print("Unexpected error:", e)
-    raise
+    traceback.print_exc()
+    sys.exit(1)
