25029aa380bb6e7d4d9dc566ec7ac2d6f6db3cfc
Nolan Welch <nolanwelch@outlook.com>
2025-07-22 10:35:59 -0700

    Add give-write-perms.sh

---
+++ scripts/compl/give-write-perms.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Sets rwx access bits on a directory for the given user
+
+usage() {
+echo "Usage: bash give-write-perms.sh <directory>"
+  exit 1
+}
+
+if [[ $# -ne 1 ]]; then usage; fi
+
+DATASET="$1"
+USER="$2"
+
+if [ ! -d "$DATASET" ]; then
+  echo "$DATASET does not exist" && exit 2
+fi
+
+if ! id "$USER" >/dev/null 2>&1; then
+  echo "$USER does not exist" && exit 3
+fi
+
+echo "Granting full access to $USER for $DATASET"
+setfacl -Rm u:"$USER":rwx "$DATASET"
+setfacl -Rdm u:"$USER":rwx "$DATASET"
+
+echo "Done!" && exit 0
